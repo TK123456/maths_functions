aebb8352b44229c341aed8365b3e0210b2d8509f
Tahir Khatri <P14148595@GWHXX584LIN002W.lec-admin.dmu.ac.uk>
2016-01-26 13:45:20 +0000

    added three files

---
+++ maths_functions.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//This program calculates the sum of x + y
+int main()
+{
+    int x = 7;
+    int y = 3;
+    printf ("%i + %i = %i\n", x, y, x+y); //adds X + Y test
+    printf ("%i + %i = %i\n", x, y, x-y); //Subtracts X-Y
+    printf ("%i + %i = %i\n", x, y, x*y); //Multiplies X*Y
+    printf ("%i + %i = %i\n", x, y, x/y); //Divides X/Y
+    return 0;
+}
