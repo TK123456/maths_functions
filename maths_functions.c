1996ea6107ec60d6a49e3a7e76842dc5ea1f1d39
Tahir Khatri <P14148595@GWHXX582LIN017W.lec-admin.dmu.ac.uk>
2016-01-27 12:46:08 +0000

    added remainder function

--- maths_functions.c
+++ maths_functions.c
@@ -3,10 +3,11 @@
 int main()
 {
     int x = 7;
-    int y = 3;
+    int y = 2;
     printf ("%i + %i = %i\n", x, y, x+y); //adds X + Y test
-    printf ("%i + %i = %i\n", x, y, x-y); //Subtracts X-Y
-    printf ("%i + %i = %i\n", x, y, x*y); //Multiplies X*Y
-    printf ("%i + %i = %i\n", x, y, x/y); //Divides X/Y
+    printf ("%i - %i = %i\n", x, y, x-y); //Subtracts X-Y
+    printf ("%i * %i = %i\n", x, y, x*y); //Multiplies X*Y
+    printf ("%i / %i = %i\n", x, y, x/y);//Divides X/Y
+    printf ("%i remainder %i = %i\n", x, y, x%y); //Remainder of x and y
     return 0;
 }
