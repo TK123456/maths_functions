95ee0822dffb33d99e1f9fdd5ab274244aaddee8
Tahir Khatri <P14148595@GWHXX582LIN017W.lec-admin.dmu.ac.uk>
2016-01-27 12:15:26 +0000

    Added testing line

--- maths_functions.c
+++ maths_functions.c
@@ -2,11 +2,15 @@
 //This program calculates the sum of x + y
 int main()
 {
-    int x = 7;
-    int y = 3;
-    printf ("%i + %i = %i\n", x, y, x+y); //adds X + Y test
-    printf ("%i + %i = %i\n", x, y, x-y); //Subtracts X-Y
-    printf ("%i + %i = %i\n", x, y, x*y); //Multiplies X*Y
-    printf ("%i + %i = %i\n", x, y, x/y); //Divides X/Y
-    return 0;
+	int x = 7;
+	int y = 2;
+	printf("%i + %i = %i\n", x, y, x+y); //adds X + Y test
+	printf("%i - %i = %i\n", x, y, x-y); //Subtracts X-Y
+	printf("%i * %i = %i\n", x, y, x*y); //Multiplies X*Y
+	printf("%i / %i = %i\n", x, y, x/y); //Divides X/Y
+	printf("%i remainder %i = %i\n", x, y, x%y); //Testing the remainder function
+	return 0;
+	
 }
+
+
